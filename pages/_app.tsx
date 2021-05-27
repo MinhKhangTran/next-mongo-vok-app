65e16ee7903d18474a507389e4726983edb57ad4
MinhKhangTran <minhkhang.tran@gmx.de>
2021-05-27 05:48:30 +0200

    :sparkles: add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Router from "next/router";
+//Nprogress
+import NProgress from "nprogress";
+import "@/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
